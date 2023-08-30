66c96fb7c083ab013efa098f1d550d2bced84f45
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-08-31 00:39:22 +0100

    add bettystyle to the file

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,6 +1,6 @@
 #include <stddef.h>
 #include "binary_trees.h"
- 
+
 /**
  * binary_tree_height - through a binary tree using pre-order traversal.
  * @tree: pointer to the root node of the tree to traverse.
@@ -8,15 +8,14 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        int left_h, right_h;
- 
-        if (tree == NULL)
-                return (0);
-        if (tree->left == NULL && tree->right == NULL)
-                return (0);
-        left_h = binary_tree_height(tree->left);
-        right_h = binary_tree_height(tree->right);
- 
-        return ((left_h > right_h ? left_h : right_h) + 1);
- 
+	int left_h, right_h;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	left_h = binary_tree_height(tree->left);
+	right_h = binary_tree_height(tree->right);
+
+	return ((left_h > right_h ? left_h : right_h) + 1);
 }
