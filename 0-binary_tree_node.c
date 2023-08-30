f48fbd19d1822a876bb33744dda68924de993c69
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-08-30 19:07:45 +0100

    create nude

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+
+/**
+* *binary_tree_node - 
+* @parent:
+* @value:
+* Return:
+*/
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+
+	if (!node)
+		return (NULL);
+
+	node->n = value;
+	node->left = NULL;
+	node->right = NULL;
+	node->parent = parent;
+	return (node);
+}
