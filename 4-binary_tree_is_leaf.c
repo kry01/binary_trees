402c772cc46d58b627c8a768d4245cd298544dc2
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-08-30 20:40:44 +0100

    [Update] fixed bettystyle

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -2,17 +2,20 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_is_leaf - check if a node is leaf
+ * @node: node to be ckecked
  *
- *
- *
- *
+ * Return: 0 if leaf, 1 if not leaf
  */
+
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
+
 	if (node == NULL)
-		return 0;
+		return (0);
 	if (node->left != NULL || node->right != NULL)
-		return 0;
+		return (0);
 	else
-		return 1;
+		return (1);
+
 }
