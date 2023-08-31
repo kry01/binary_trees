3c956df67951461cda48bdac2e81d3b3237593bc
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-09-01 00:51:28 +0100

    is uncle

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle -  finds the uncle of a node.
+ * @node: the node to know its uncle.
+ * Return: the sibling or NULL.
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *grandparent;
+
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
+
+	grandparent = node->parent->parent;
+
+	if (grandparent->left == node->parent)
+		return (grandparent->right);
+	else
+		return (grandparent->left);
+}
