db382cb9f78c07a743e15b42ff09c4080ec44f11
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-09-01 00:32:35 +0100

    [Update] fixed bettystyle

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,14 +8,11 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (node == NULL)
-		return (NULL);
-	if (node->parent == NULL)
+	if (node == NULL || node->parent == NULL)
 		return (NULL);
 	if (node->parent->left == NULL || node->parent->right == NULL)
 		return (NULL);
 	if (node == node->parent->left)
 		return (node->parent->right);
-	else
-		return (node->parent->left);
+	return (node->parent->left);
 }
