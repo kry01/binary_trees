1a2a872945e28d91e27d0c5f5123a4393918436a
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-08-31 23:44:17 +0100

    add task 16

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,37 @@
+#include "binary_trees.h"
+#include <stddef.h>
+
+/**
+ * binary_tree_nodes - counts the nodes with at least one child in a BT.
+ * @tree: Pointer to the node to count
+ * Return: If tree is NULL, must return 0
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	int lleft, lright;
+
+	if (tree == NULL)
+		return (0);
+	lleft = binary_tree_nodes(tree->left);
+	lright = binary_tree_nodes(tree->right);
+	return (lleft + lright + 1);
+}
+
+/**
+ * binary_tree_is_full -  checks if a binary tree is full.
+ * @tree:  pointer to the root node of the tree to check.
+ * Return: 0 or 1.
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (binary_tree_nodes(tree) % 2 != 0)
+	{
+		if (binary_tree_nodes(tree->left) == binary_tree_nodes(tree->right))
+					return (1);
+	}
+	return (0);
+}
