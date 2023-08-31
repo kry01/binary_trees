3e10c58b6284a9ed1cb76c414ae623f5871e2389
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-08-31 21:10:25 +0100

    balance factor

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,37 @@
+#include <stddef.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - through a binary tree using pre-order traversal.
+ * @tree: pointer to the root node of the tree to traverse.
+ * Return: 0 or increment by 1.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int left_h, right_h;
+
+	if (tree == NULL)
+		return (-1);
+	left_h = binary_tree_height(tree->left);
+	right_h = binary_tree_height(tree->right);
+	return ((left_h > right_h ? left_h : right_h) + 1);
+}
+
+
+
+/**
+ * binary_tree_balance - the balance factor of a binary tree.
+ * @tree: pointer to the root node of the tree to traverse.
+ * Return: 0 or increment by 1.
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left_h, right_h = 0;
+
+	if (tree == NULL)
+		return (0);
+	left_h = binary_tree_height(tree->left);
+	right_h = binary_tree_height(tree->right);
+
+	return (left_h - right_h);
+}
