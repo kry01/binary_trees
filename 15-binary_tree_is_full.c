dd5812ad1681fb8191dd4e4112a599ef59a428ed
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-08-31 21:23:19 +0100

    add task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -18,11 +18,17 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	return (lleft + lright + 1);
 }
 
+/**
+ * binary_tree_is_full -  checks if a binary tree is full.
+ * @tree:  pointer to the root node of the tree to check.
+ * Return: 0 or 1.
+ */
+
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if(binary_tree_nodes(tree) % 2 != 0)
+	if (binary_tree_nodes(tree) % 2 != 0)
 		return (1);
 	return (0);
 }
