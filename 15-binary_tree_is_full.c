a75980bba11c6dade776e2519745083bb3392f21
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-08-31 21:21:02 +0100

    full tree

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+#include <stddef.h>
+
+/**
+ * binary_tree_nodes - counts the nodes with at least one child in a BT.
+ * @tree: Pointer to the node to count
+ * Return: If tree is NULL, must return 0
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	int lleft, lright;
+
+	if (tree == NULL)
+		return (0);
+	lleft = binary_tree_nodes(tree->left);
+	lright = binary_tree_nodes(tree->right);
+	return (lleft + lright + 1);
+}
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if(binary_tree_nodes(tree) % 2 != 0)
+		return (1);
+	return (0);
+}
